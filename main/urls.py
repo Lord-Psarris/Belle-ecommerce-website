00dbfe1ed045ac8ea0ac03c6ba13f3088a268a99
Lord-Psarris <theorchestra03@gmail.com>
2022-04-08 11:08:34 +0100

    integrated search and newsletter

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,5 @@ app_name = 'main'
 urlpatterns = [
     path('', views.home, name='home'),
     path('search/', views.search, name='search'),
+    path('subscribe-to-newsletter/', views.subscibe_to_newsletter, name='subscibe_to_newsletter'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,11 @@
 import json
 
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+
+from main.models import NewsletterSubscriber
+from products.models import Product
 from products.reuseable_functions import get_on_sale_products, get_best_selling_products, get_newly_arrived_products, \
     get_products_based_on_category
 
@@ -23,9 +28,51 @@ def home(request):
 def search(request):
     search_query = request.GET.get('query')
     categories = ['clothing', 'cosmetics', 'bags', 'shoes', 'accessories', 'jewellery']
-    print(search_query)
 
     if search_query in categories:
         results = get_products_based_on_category(search_query)
+    else:
+        all_product_results = Product.objects.filter(name__icontains=search_query).all()
+        results = []
+
+        if all_product_results is not None:
+            for product in all_product_results:
+                item = {
+                    'pk': product.id,
+                    'name': product.name,
+                    'short_name': product.get_short_name(),
+                    'price': product.price,
+                    'in_stock': product.in_stock,
+                    'category': product.category,
+                    'out_of_stock': True if product.in_stock == 0 else False,
+                    'colors_available': json.loads(str(product.colors_available).strip("' ").replace('\'', '\"')),
+                    'sizes_available': json.loads(str(product.sizes_available).strip("' ").replace('\'', '\"')),
+                    'description': product.description,
+                    'has_discount': product.has_discount,
+                    'is_new': product.is_new,
+                    'discounted_percent': product.discounted_percent,
+                    'slashed_price': product.price - round(product.price * (product.discounted_percent / 100)),
+                    'sales_count': product.sales_count,
+                    'image1': product.productimage_set.all()[0].file,
+                    'image2': product.productimage_set.all()[1].file,
+                }
+                if item['out_of_stock'] is False:
+                    results.append(item)
 
     return render(request, 'main/search.html', {'search': search_query, 'results': results})
+
+
+def subscibe_to_newsletter(request):
+    email = request.POST.get('email')
+    return_url = request.GET.get('return_url')
+
+    old_subscriber = NewsletterSubscriber.objects.filter(email=email).first()
+    if old_subscriber is None:
+        new_subscriber = NewsletterSubscriber(email=email)
+        new_subscriber.save()
+
+        messages.success(request, 'Thank you for subscribing to our newsletter')
+        return HttpResponseRedirect(return_url)
+
+    messages.error(request, 'You have already subscribed')
+    return HttpResponseRedirect(return_url)
